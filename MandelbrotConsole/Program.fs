aff033aa2482a1580c3623f2135a27800fc97f71
J. Wostenberg <>
2012-12-29 13:52:41 -0700

    Program now draws a mandelbrot set correctly

--- MandelbrotConsole/Program.fs
+++ MandelbrotConsole/Program.fs
@@ -4,8 +4,8 @@ open MandelbrotGenerator
 
 [<EntryPoint>]
 let main args = 
-  for x in (-2.0)..(0.1)..(2.0) do
-    for y in (-2.0)..(0.1)..(2.0) do
+  for y in (-1.0)..(0.05)..(1.0) do
+    for x in (-2.0)..(0.0315)..(0.5) do
       if isMandelbrot (new Complex(x, y)) then
         printf "*"
       else
