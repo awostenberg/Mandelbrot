cc6d17f4617f5f81278c8b38d249ae72d2872850
J. Wostenberg <alanwostenberg@alan-wostenbergs-imac.home>
2012-12-29 13:05:33 -0700

    Defined the mandelbrot function. I _think_ it works; no way to be sure until an image is drawn...

--- Mandelbrot/MandelbrotGenerator.fs
+++ Mandelbrot/MandelbrotGenerator.fs
@@ -2,5 +2,10 @@ module MandelbrotGenerator
 open System.Numerics
 
 // Calculates wether or not the Mandelbrot set contains the complex point
-let isMandelbrot complex =
-  true
+let isMandelbrot c =
+  let rec mandelbrotOf (oldComplex : Complex) (complex : Complex) maxI i =
+    if i < maxI then
+      mandelbrotOf oldComplex (complex * complex + oldComplex) maxI (i + 1)
+    else
+      complex
+  abs ((mandelbrotOf c c 10 0).Magnitude) > 2.0
