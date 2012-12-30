6e242ef4b8a19626fb5839d8fe2d1e405fe7ca35
J. Wostenberg <>
2012-12-29 17:07:31 -0700

    Added x, y, and zoom arguments

--- MandelbrotConsole/Program.fs
+++ MandelbrotConsole/Program.fs
@@ -2,12 +2,13 @@ module MandelbrotConsole.Main
 open System.Numerics
 open MandelbrotGenerator
 
+let getFloatArg (a : string []) i defaultVal =
+  if (a.Length - 1) > i then
+    a.[0] |> float
+  else
+    defaultVal
+
 [<EntryPoint>]
 let main args =
-  let zoom =
-    if args.Length > 0 then
-      args.[0] |> float |> abs
-    else
-      1.0
-  renderZebraMandelbrotSet255x255 zoom |> ignore
+  renderZebraMandelbrotSet255x255 (getFloatArg args 0 0.0) (getFloatArg args 1 0.0) (getFloatArg args 2 1.0)
   0
