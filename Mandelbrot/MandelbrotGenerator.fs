9e4a7e9398e83d19fb956814a19a8e8e3a942b98
J. Wostenberg <>
2013-01-01 16:00:16 -0700

    It now renders onto a 2D Array instead of just printing the values directly. The set appears sideways though...

--- Mandelbrot/MandelbrotGenerator.fs
+++ Mandelbrot/MandelbrotGenerator.fs
@@ -12,12 +12,16 @@ let isMandelbrot c =
 
 // Renders a B/W mandelbrot set onto a 2D Array of true/false values
 let renderZebraMandelbrotSet cx cy zoom =
-  let xStart, xStep, xEnd = -2.0 / zoom + cx, 0.0315 / zoom, 0.5 / zoom + cx
-  let yStart, yStep, yEnd = -1.0 / zoom + cy, 0.05 / zoom, 1.0 / zoom + cy
-  for y in yStart..yStep..yEnd do
-    for x in xStart..xStep..xEnd do
-      if isMandelbrot (new Complex(x, y)) then
-        printf "*"
-      else
-        printf " "
-    printfn ""
+  let xStart, xStep, xEnd = -2.0 / zoom + cx, 0.05 / zoom, 0.5 / zoom + cx
+  let yStart, yStep, yEnd = -1.0 / zoom + cy, 0.0315 / zoom, 1.0 / zoom + cy
+  let xw, yw = round xStart + xEnd / xStep |> int, round yStart + yEnd / yStep |> int
+  let ary = Array2D.zeroCreate<bool> (abs ((xStart - xEnd) / xStep) + 1.0 |> int) (abs ((yStart - yEnd) / yStep) + 1.0 |> int)//(round xStart + xEnd / xStep |> int) (round yStart + yEnd / yStep |> int)
+  for x in xStart..xStep..xEnd do
+    for y in yStart..yStep..yEnd do
+      let rx, ry = (x - xStart) * (1.0 / xStep) |> int, (y - yStart) * (1.0 / yStep) |> int
+      //if not (rx = round rx) then printfn "WARNING: BAD X COMING UP"
+      //if not (ry = round ry) then printfn "WARNING: BAD Y COMING UP"
+      //printfn "(%f, %f) -> (%f, %f)" x y rx ry
+      ary.[rx, ry] <- isMandelbrot (new Complex(x, y))
+    //printfn ""
+  ary
--- MandelbrotConsole/Program.fs
+++ MandelbrotConsole/Program.fs
@@ -10,5 +10,9 @@ let getFloatArg (args : string []) i defaultVal =
 
 [<EntryPoint>]
 let main args =
-  renderZebraMandelbrotSet (getFloatArg args 0 0.0) (getFloatArg args 1 0.0) (getFloatArg args 2 1.0)
+  let data = renderZebraMandelbrotSet (getFloatArg args 0 0.0) (getFloatArg args 1 0.0) (getFloatArg args 2 1.0)
+  for x in 0..(Array2D.length1 data - 1) do
+    for y in 0..(Array2D.length2 data - 1) do
+      if data.[x, y] then printf "*" else printf " "
+    printfn ""
   0
